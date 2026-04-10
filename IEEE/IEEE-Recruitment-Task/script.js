a79947c1fd0326f623d9c5bbab95e4842141c6bb
nishant01-webdvr <sahunishant230@gmail.com>
2026-04-10 18:11:00 +0530

    Add files via upload

---
+++ IEEE/IEEE-Recruitment-Task/script.js
@@ -0,0 +1,12 @@
+
+document.querySelector(".menu").addEventListener("click",()=>{
+ document.querySelector(".mobilenav").style.left="0"
+
+
+    })
+document.querySelector(".close").addEventListener("click",()=>{
+   document.querySelector(".mobilenav").style.left="-110%"
+  })
+
+
+
